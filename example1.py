34e4c16bb0ca137890ec368e4fe9ad117473f3e9
Leo-Simpson <52931376+Leo-Simpson@users.noreply.github.com>
2020-01-14 01:12:21 +0100

    Create example1.py

---
+++ example1.py
@@ -0,0 +1,6 @@
+from CLasso import *
+m,d,d_nonzero,k,sigma =100,100,5,1,0.5
+(X,C,y),sol = random_data(m,d,d_nonzero,k,sigma,zerosum=True)
+problem = classo_problem(X,y,C)
+problem.solve()
+print(problem.solution)
