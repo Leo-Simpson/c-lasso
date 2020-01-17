48d23d84944b6be5844c3c0c08ef37baa62ac6db
Leo-Simpson <52931376+Leo-Simpson@users.noreply.github.com>
2020-01-17 13:13:28 +0100

    Modification of 14.01.2020

--- example1.py
+++ example1.py
@@ -2,6 +2,6 @@ from CLasso import *
 m,d,d_nonzero,k,sigma =100,100,5,1,0.5
 (X,C,y),sol = random_data(m,d,d_nonzero,k,sigma,zerosum=True)
 problem = classo_problem(X,y,C)
-print(problem)
 problem.solve()
+print(problem)
 print(problem.solution)
--- example2.py
+++ example2.py
@@ -10,8 +10,8 @@ problem.model_selection.CV                  = True
 problem.model_selection.LAMfixed            = True
 problem.model_selection.SSparameters.method = 'max'
 
-print(problem)
 problem.solve()
+print(problem)
 print(problem.solution)
 
 problem.solution.CV.graphic(mse_max = 1.)
