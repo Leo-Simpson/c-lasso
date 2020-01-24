f60704620866b9f7883781feb3d2be52c02bad6e
Leo-Simpson <52931376+Leo-Simpson@users.noreply.github.com>
2020-01-24 14:41:36 +0100

    Update __init__.py

--- CLasso/__init__.py
+++ CLasso/__init__.py
@@ -1,4 +1,4 @@
-from CLasso.little_functions import random_data, csv_to_mat, rescale,clr, theoritical_lam
+from CLasso.little_functions import random_data, csv_to_mat, mat_to_np, rescale,clr, theoritical_lam
 import scipy.io as sio
 from CLasso.compact_func import Classo, pathlasso
 from CLasso.cross_validation import CV
