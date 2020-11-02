8936a9c5d4299e24a5d7639bc3b5bea9eb7bf942
Leo-Simpson <52931376+Leo-Simpson@users.noreply.github.com>
2020-11-02 09:01:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='c-lasso',
-      version='0.3.1.77',
+      version='1.0',
       license='MIT',
       author='Leo Simpson',
       url='https://github.com/Leo-Simpson/CLasso',
